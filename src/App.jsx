e508dd16db42a235e5452877936544d74c143165
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2021-11-09 19:34:14 +0530

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 // Imports
 import Web3ContextProvider from "./context/Web3Context";
-
+import { Routes, Route, Navigate } from "react-router";
 // Components
-
 import Navbar from "./components/Navbar";
+import Verify from "./components/Verify";
+import View from "./components/View";
+import Create from "./components/Create";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
 
 // Styles
 import "./styles/App.scss";
@@ -12,7 +16,20 @@ const App = () => {
 	return (
 		<Web3ContextProvider>
 			<Navbar />
-			<main>Hello</main>
+
+			<main>
+				<Routes>
+					<Route path="/">
+						<Route exact path="verify" element={<Verify />} />
+						<Route exact path="create" element={<Create />} />
+						<Route exact path="view" element={<View />} />
+						<Route exact path="" element={<Home />} />
+						<Route exact path="404" element={<NotFound />} />
+						<Route path="*" element={<Navigate to="/404" />} />
+					</Route>
+				</Routes>
+			</main>
+
 			<footer>
 				<div>BUILT with &lt;3</div>
 			</footer>
