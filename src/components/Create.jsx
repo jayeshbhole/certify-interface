3b498e6b3e14b9c5c2cef6824d3994cdfe356618
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2021-11-10 14:24:26 +0530

    Code Split - Create Single Certificate

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,123 +1,13 @@
-import React from "react";
-import { useForm, useWatch } from "react-hook-form";
+import SingleCertificate from "./SingleCertificate";
 import "../styles/create.scss";
 
 const Create = () => {
-	const {
-		register,
-		handleSubmit,
-		control,
-		formState: { errors },
-	} = useForm();
-
-	const onSubmit = (data) => {
-		console.log(data);
-		console.log(errors);
-	};
-
-	const con = {
-		required: { value: true, message: "This field is Required" },
-		minLength: { value: 3, message: "At least 3 characters required" },
-	};
-
 	return (
 		<div className="create">
-			<div className="single">
-				<div className="txt">
-					<h2>Issue Certificates</h2>
-					<div className="inner">
-						<span>
-							Issue certificates with the form manually or import CSV files.
-							Fill out the adjacent form and create a new certificate.
-						</span>
-						<span>
-							Pay minimal transaction fee and get a copy of the certificate
-							Hash.
-						</span>
-						<span>
-							Refer to the CSV file format below before importing data.
-						</span>
-					</div>
-					<hr />
-					<Code control={control} />
-				</div>
-
-				<form onSubmit={handleSubmit(onSubmit)}>
-					<Input
-						name="BeneficiaryName"
-						label="Beneficiary Name"
-						type="text"
-						conditions={con}
-						register={register}
-						errors={errors}
-					/>
-					<Input
-						name="CertificateDescription"
-						label="Certificate Description"
-						type="text"
-						conditions={con}
-						register={register}
-						errors={errors}
-					/>
-					<Input
-						name="ExpirationDate"
-						label="Expiration Date"
-						type="date"
-						conditions={{ required: true }}
-						register={register}
-						errors={errors}
-						value={"1970-01-01"}
-					/>
-					<Input
-						name="InstituteAuthorityName"
-						label="Institute/ Authority Name"
-						type="text"
-						conditions={con}
-						register={register}
-						errors={errors}
-					/>
-					<div className="item">
-						<label>Additional Notes</label>
-						{errors["Additional Notes"] && <span>Error</span>}
-						<textarea
-							name="AdditionalNotes"
-							label="Additional Notes"
-							type="textarea"
-							{...register("AdditionalNotes", { min: 3 })}
-							errors={errors}
-						/>
-					</div>
-					<input type="submit" value="Issue Certificate" />
-				</form>
-			</div>
+			<SingleCertificate />
 			<div className="batch"></div>
 		</div>
 	);
 };
-
-const Code = ({ control }) => {
-	const watchedAllFields = useWatch({ control });
-	return (
-		<div className="code">
-			Certificate JSON
-			<pre>{JSON.stringify(watchedAllFields, null, 2)}</pre>
-		</div>
-	);
-};
-
-const Input = ({ name, label, type, register, conditions, errors, value }) => (
-	<div className="item">
-		<label>{label}</label>{" "}
-		<span className="error">
-			{errors[name] && <span>{errors[name]?.message}</span>}
-		</span>
-		<input
-			{...register(name, conditions)}
-			type={type}
-			name={name}
-			value={value}
-		/>
-	</div>
-);
 
 export default Create;
