110ef9f0b432bf5064a1b485b67fb6e853cd6620
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2021-11-11 15:57:02 +0530

    Verify Page - Form added

--- src/components/Verify.jsx
+++ src/components/Verify.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { lazy, Suspense } from "react";
+import { useSearchParams } from "react-router-dom";
+import Certificate from "./Certificate";
+import PageLoader from "./PageLoader";
+
+const VerifyForm = lazy(() => import("./VerifyForm"));
 
 const Verify = () => {
-	return <div>Verify</div>;
+	const [searchParams] = useSearchParams();
+
+	return !!searchParams.has("certkey") & !!searchParams.has("ipfsHash") ? (
+		<div className="view">
+			<Certificate
+				certkey={searchParams.get("certkey")}
+				ipfsHash={searchParams.get("ipfsHash")}
+			/>
+		</div>
+	) : (
+		<Suspense fallback={<PageLoader />}>
+			<VerifyForm />
+		</Suspense>
+	);
 };
 
 export default Verify;
---
+++ src/components/VerifyForm.jsx
@@ -0,0 +1,55 @@
+import { useSearchParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import "../styles/verify.scss";
+
+const VerifyForm = () => {
+	const [_, setSearchParams] = useSearchParams();
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+
+	const onSubmit = (data) => {
+		const params = { certkey: data.certkey, ipfsHash: data.ipfsHash };
+		setSearchParams(params);
+	};
+	return (
+		<div className="verify">
+			<h2>Verify Certificate</h2>
+			<br />
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<div className="item">
+					<label>IPFS Hash (Address)</label>{" "}
+					<span className="error">
+						{errors["ipfsHash"] && <span>{errors["ipfsHash"]?.message}</span>}
+					</span>
+					<input
+						{...register("ipfsHash", {
+							required: { value: true, message: "This field is Required" },
+						})}
+						type="text"
+						name="ipfsHash"
+					/>
+				</div>
+				<div className="item">
+					<label>Certificate Key</label>{" "}
+					<span className="error">
+						{errors["certkey"] && <span>{errors["certkey"]?.message}</span>}
+					</span>
+					<input
+						{...register("certkey", {
+							required: { value: true, message: "This field is Required" },
+						})}
+						type="text"
+						name="certkey"
+					/>
+				</div>
+				<input type="submit" value="Verify Certificate" />
+			</form>
+		</div>
+	);
+};
+
+export default VerifyForm;
