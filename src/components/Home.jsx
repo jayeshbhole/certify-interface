5ffe660204ed31a3caa5d79dc0efa57febed35c6
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2021-11-27 19:41:45 +0530

    Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,29 @@
-import HomeSVG from "../assets/home.svg";
-
+import { Link } from "react-router-dom";
+import "../styles/Home.scss";
 const Home = () => {
 	return (
-		<div style={{ textAlign: "center" }}>
-			<img src={HomeSVG} alt="" />
+		<div className="home">
+			<h1 style={{ fontWeight: 400 }}>
+				Create And Verify Immutable Certificates & Degrees
+			</h1>
+			<img src="/art-1.png" alt="certify logo" />
+			<div className="info">
+				Built on the Celo blockchain, For Security And Trust
+				<br />
+				<div className="links">
+					<Link role="button" to="/create">
+						Create
+					</Link>
+					<Link role="button" to="/verify">
+						Verify
+					</Link>
+					<Link role="button" to="/actions">
+						Actions
+					</Link>
+				</div>
+			</div>
+
+			{/* <img src={HomeSVG} alt="" /> */}
 		</div>
 	);
 };
