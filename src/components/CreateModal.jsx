4e967418c0f408f47321559a4140cb360cd71dd9
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2021-11-27 21:06:42 +0530

    Update CreateModal.jsx

--- src/components/CreateModal.jsx
+++ src/components/CreateModal.jsx
@@ -37,7 +37,7 @@ const Modal = ({ modalData: { mode, data, receipt }, setModalData }) => {
 			transactionHash: receipt?.transactionHash,
 			ipfsHash: data?.ipfsHash,
 			certkey: data?.certkey,
-			shareableLink: `https://certify-v2.netlify.app/verify/?ipfsHash=${data?.ipfsHash}&certkey=${data?.certkey}`,
+			shareableLink: `https://certify-v1.netlify.app/verify/?ipfsHash=${data?.ipfsHash}&certkey=${data?.certkey}`,
 		};
 		const blob = new Blob([JSON.stringify(obj, null, "\t")], {
 			type: "application/json",
